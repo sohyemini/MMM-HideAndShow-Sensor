8ba48dd29fd2ada95497cf21f95c8bb5c3da2e83
소혜민 <sohyemini@gmail.com>
2020-09-10 23:37:18 +0900

    PIR 센서 테스트 코드

--- MMM-HideAndShow-Sensor.js
+++ MMM-HideAndShow-Sensor.js
@@ -7,13 +7,52 @@
 
 Module.register('MMM-HideAndShow-Sensor', {
 	start: function () {		
+		console.log('MMM-HASS / Start : ' + this.name);
 		this.sendSocketNotification('CONFIG', this.config);
-		Log.info('MMM-HASS / Start : ' + this.name);
 	},
 
 	socketNotificationReceived: function (notification, payload) {
 		if (notification === 'SHOW_ALERT') {
+			console.log('MMM-HASS / MMM-HideAndShow-Sensor');
 			this.sendNotification(notification, payload);
+		} 
+		
+		if (notification === "HIDE_MODULES") {
+			config.bShow = false;
+			MM.getModules().withClass(['newsfeed', 
+                                      'weatherforecast', 
+									  'MMM-AirQuality',
+									  'currentweather',
+									  'MMM-GmailFeed',
+									  'calendar',
+									  'clock',
+									  //'MMM-BackgroundSlideshow',
+									  'MMM-GooglePhotos',
+									  'currentweather'])
+                                .enumerate(function(module){
+                                    module.hide();
+                                });
+		    console.log('MMM-HASS : turn off....in main js');
+		    this.sendNotification(notification, payload);
 		}
+		
+		if (notification === "SHOW_MODULES") {
+			config.bShow = true;
+			MM.getModules().withClass(['newsfeed', 
+                                      'weatherforecast', 
+									  'MMM-AirQuality',
+									  'currentweather',
+									  'MMM-GmailFeed',
+									  'calendar',
+									  'clock',
+									  //'MMM-BackgroundSlideshow',
+									  'MMM-GooglePhotos',
+									  'currentweather'])
+                                .enumerate(function(module){
+                                    module.show();
+                                });
+		    console.log('MMM-HASS : turn on....in main js');
+		    this.sendNotification(notification, payload);
+		}		
 	},
 });
--- node_helper.js
+++ node_helper.js
@@ -12,16 +12,40 @@ const NodeHelper = require('node_helper');
 module.exports = NodeHelper.create({	
     start: function () 
     {
+	    var self = this;
+	    var x = setInterval(function(){
+		console.log('MMM-HASS : config.iMotion = config.iMotion - 1 / iMotion = ' + config.iMotion);
+		config.iMotion = config.iMotion - 1;
+		if (config.iMotion === 0)
+		{
+		    self.sendSocketNotification("HIDE_MODULES",{});
+		    /*getModules().withClass(['newsfeed', 
+                                                          'weatherforecast', 
+                                                          'MMM-AirQuality',
+                                                          'currentweather',
+                                                          'MMM-GmailFeed',
+                                                          'calendar',
+                                                          'clock',
+                                                          'currentweather'])
+                                .enumerate(function(module){
+                                    module.hide();
+                                });
+		    console.log('MMM-HASS : turn off....');
+		    * */
+		}
+	    }, 1000);
 	    console.log('MMM-HASS : start node_helper ');
 	    this.started = false;
     }, // end of start function
 
     socketNotificationReceived: function (notification, payload) 
     {
+	    var self = this;
 	    if (notification === 'CONFIG' && this.started == false) 
 	    {
 		    const self = this;
 		    this.config = payload;
+		    console.log('MMM-HASS / socketNotificationReceived, CONFIG, this.started.......');
 		    // GPIO의 Pin 지정
 		    this.pir = new Gpio(this.config.pin, 'in', 'both');
 		    // GPIO로 부터 값 읽기
@@ -32,13 +56,24 @@ module.exports = NodeHelper.create({
 			    if(value)
 			    {
 				    //Notification 생성, SHOW_ALERT을 통해서 화면에 Toast 출력
-				    self.sendSocketNotification('SHOW_ALERT', 
+				  /*  self.sendSocketNotification('SHOW_ALERT', 
 				    {
 					    title: 'PIR',
 					    message: 'Motion detected!',
-					    timer: 2500
+					    timer: 1000
 				    }); // end of sendSocketNotification
+				   */ console.log('MMM-HASS / PIR 이벤트 받음 iMotion = ' + config.iMotion);
+				    config.iMotion = 12;//600;
+				    bShow = true;
+				    self.sendSocketNotification("SHOW_MODULES",{});
 			    } // end of if
+			    /*else
+			    {
+				config.iMotion = config.iMotion -1 ;
+				if(config.iMotion === 0)
+				    console.log('MMM-HASS / turn off screen');
+				console.log('MMM-HASS / iMotion = ' + config.iMotion);
+			    }*/
 		    }); // end of watch
 		    this.started = true;
 	    } // end of if
