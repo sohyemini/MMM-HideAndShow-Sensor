629a2dec621d66748581716470e8edb3fe1373da
sohyemini <71050116+sohyemini@users.noreply.github.com>
2020-09-10 12:43:58 +0900

    Update MMM-HideAndShow-Sensor.js

--- MMM-HideAndShow-Sensor.js
+++ MMM-HideAndShow-Sensor.js
@@ -1,1 +1,19 @@
+/****************************************************************/
+/*                   MMM-HideAndShow-Sensor                     */
+/*                                                              */
+/*                            Sept 10, 2020                     */
+/*                     Written by Sohyemini                     */
+/****************************************************************/
 
+Module.register('MMM-HideAndShow-Sensor', {
+	start: function () {		
+		this.sendSocketNotification('CONFIG', this.config);
+		Log.info('MMM-HASS / Start : ' + this.name);
+	},
+
+	socketNotificationReceived: function (notification, payload) {
+		if (notification === 'MOTHION_DETECTED') {
+			this.sendNotification(notification, payload);
+		}
+	},
+});
